59ca6dec2894db36254897e1bc00279c3173ff96
Bernardo Trindade <btrindad@mines.edu>
2014-04-17 10:09:52 -0600

    added test for searcher name

--- test/SearcherTest.java
+++ test/SearcherTest.java
@@ -1,14 +1,39 @@
 package test;
 
 import static org.junit.Assert.*;
+import main.Dog;
+import main.Helicopter;
+import main.Hiker;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SearcherTest {
+	private Hiker hiker;
+	private Dog dog;
+	private Helicopter helicopter;
+	
+	@BeforeClass
+	public void setUp(){
+		hiker = new Hiker();
+		dog = new Dog();
+		helicopter = new Helicopter();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void searchersHaveName(){
+		String hikerName = "Hiker1";
+		String dogName = "Dog1";
+		String heliName = "Helicopter1";
+		
+		hiker.setName(hikerName);
+		dog.setName(dogName);
+		helicopter.setName(heliName);
+		
+		assertEquals(hikerName, hiker.getName());
+		assertEquals(dogName, dog.getName());
+		assertEquals(heliName, helicopter.getName());
+		
 	}
 
 }
