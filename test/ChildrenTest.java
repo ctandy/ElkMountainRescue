5ea2a1d52a8806adf6ca71342fe5ed34019a5622
mberntso <mberntso@mines.edu>
2014-04-16 21:22:44 -0600

    Added tests.

---
+++ test/ChildrenTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChildrenTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
