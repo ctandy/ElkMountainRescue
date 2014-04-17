dcca9e5f3c7025f721ef2fdde531595feddd4ade
mberntso <mberntso@mines.edu>
2014-04-17 10:17:44 -0600

    Minor fixes

--- main/Dog.java
+++ main/Dog.java
@@ -5,7 +5,7 @@ import java.awt.Graphics;
 public class Dog extends Searcher {
 	private static double RADIUS = 0.0;  
 	
-	public Dog(String n, double s, double dir, int ind){
+	public Dog(String n, double s, double dir, Cell ind){
 		this.setName(n);
 		this.setSpeed(s);
 		this.setDirection(dir);
