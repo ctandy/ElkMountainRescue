7ea9c96ae11f041960ba77e5b291f3f1feab092d
mberntso <mberntso@mines.edu>
2014-04-22 12:01:50 -0600

    Moved move and addSearcher and Search aray to Grid, should help with painting. Put a speed limiter on the move function.

--- main/Grid.java
+++ main/Grid.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import javax.swing.JPanel;
 
 public class Grid extends JPanel{
+	private ArrayList<Searcher> searchers;
 	public final static Integer CELL_SIZE = 16;
 	private ArrayList<Cell> cells;
 	//origin of grid is upper-left
@@ -25,6 +26,7 @@ public class Grid extends JPanel{
 	public Grid() throws BadConfigFormatException{
 		this.loadConfigFiles();
 		this.initializeCells();
+		searchers = new ArrayList<Searcher>();
 	}
 	
 	public void initializeCells(){
@@ -37,10 +39,6 @@ public class Grid extends JPanel{
 	}
 	
 	public void loadConfigFiles() throws BadConfigFormatException{
-		int mrow = 5; //set mrow and mcol for testing
-		int mcol = 4;
-		//this.setMAX_ROW(mrow);
-		//this.setMAX_COL(mcol);
 		importBackground();
 		this.setMAX_ROW(map.getHeight(this)/CELL_SIZE);
 		this.setMAX_COL(map.getWidth(this)/CELL_SIZE);
@@ -72,12 +70,45 @@ public class Grid extends JPanel{
 	    map = original;
 	}
 	
+	public void move(Searcher s){
+		Cell cell = s.getIndex();
+		int introw = cell.getRow();
+		int intcol = cell.getCol();
+		int newrow, newcol;
+		while(true){
+			int dx = (int) (s.getSpeed()*Math.cos(Math.toRadians(s.getDirection())));
+			int dy = (int) (s.getSpeed()*Math.sin(Math.toRadians(s.getDirection())));
+			newrow = introw-dy;
+			newcol = intcol+dx;
+			if (s.getSpeed() > Math.sqrt(Math.pow(Grid.MAX_COL, 2)+Math.pow(Grid.MAX_ROW, 2))){
+				s.setSpeed(s.getSpeed()/2);
+			}
+			else if ((newrow<0 || newrow>Grid.MAX_ROW-1) || (newcol<0 || newcol>Grid.MAX_COL-1)){
+				s.setDirection(Math.random()*360);
+			}
+			else {
+				cell.setRow(newrow);
+				cell.setCol(newcol);
+				s.setIndex(cell);
+				break;
+			}
+		}
+	}
+	
+	public void addSearcher(Searcher s){ //called from a MouseListener for the 'add' button
+		this.getSearchers().add(s);
+	}
+	
 	public void setMAX_ROW(int mAX_ROW) {
 		MAX_ROW = mAX_ROW;
 	}
 
 	public void setMAX_COL(int mAX_COL) {
 		MAX_COL = mAX_COL;
+	}
+	
+	public ArrayList<Searcher> getSearchers() {
+		return searchers;
 	}
 	
 	public ArrayList<Cell> getCells() {
--- main/Rescue.java
+++ main/Rescue.java
@@ -1,49 +1,15 @@
 package main;
 
-import java.awt.Graphics;
 import java.awt.event.MouseListener;
-import java.util.ArrayList;
 
 public class Rescue {
-	private ArrayList<Searcher> searchers;
 	private Grid grid;
 
 	public Rescue() throws BadConfigFormatException{
-		searchers = new ArrayList<Searcher>();
 		grid = new Grid();
 	}
 	
-	public void addSearcher(Searcher s){ //called from a MouseListener for the 'add' button
-		this.getSearchers().add(s);
-	}
-	
-	public void move(Searcher s){
-		Cell cell = s.getIndex();
-		int introw = cell.getRow();
-		int intcol = cell.getCol();
-		int newrow, newcol;
-		while(true){
-			int dx = (int) (s.getSpeed()*Math.cos(Math.toRadians(s.getDirection())));
-			int dy = (int) (s.getSpeed()*Math.sin(Math.toRadians(s.getDirection())));
-			newrow = introw-dy;
-			newcol = intcol+dx;
-			if ((newrow<0 || newrow>Grid.MAX_ROW-1) || (newcol<0 || newcol>Grid.MAX_COL-1)){
-				s.setDirection(Math.random()*360);
-			}
-			else {
-				cell.setRow(newrow);
-				cell.setCol(newcol);
-				s.setIndex(cell);
-				break;
-			}
-		}
-	}
-	
-	public void manualUpdate(MouseListener click){} //Part of GUI, done in part 2 
-
-	public ArrayList<Searcher> getSearchers() {
-		return searchers;
-	}
+	public void manualUpdate(MouseListener click){} 
 	
 	public Grid getGrid(){
 		return grid;
